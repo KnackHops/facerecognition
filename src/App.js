0aeb2b5e01a6b95b9475c6d7855f202b6df99408
Aldrian <himalosc@gmail.com>
2020-04-08 12:15:18 +0800

    cleaner

--- src/App.js
+++ src/App.js
@@ -52,11 +52,9 @@ class App extends Component {
     this.setState({inputURL: this.state.input})
 
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      (response) => {
+      (response) =>
         console.log(response.rawData.outputs[0].data.regions[0].region_info.bounding_box)
-      }).catch(err => {
-        console.log(err);
-      });
+      ).catch(err => console.log(err));
   }
 
   render() {
