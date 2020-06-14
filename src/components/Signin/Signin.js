e6f73e3ab8e733a3725ca06c4e786dccfe0865fa
Aldrian <himalosc@gmail.com>
2020-06-14 22:29:29 +0800

    added error message on signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -6,7 +6,10 @@ class Signin extends React.Component {
         super(props);
         this.state = {
             logInUsername: "",
-            logInPassword: ""
+            logInPassword: "",
+            logInStatus: "",
+            logInColor: "",
+            boxParam: 0
         }
     }
 
@@ -31,15 +34,37 @@ class Signin extends React.Component {
         .then(data=>{
             if(data==="success"){
                 this.props.onRouteChange("home");
+            }else{
+                this.logInStatFail(data);
             }
         } 
         )
         
     }
 
+    logInStatFail = data =>{
+        this.setState({
+            logInStatus:data,
+            logInColor: "rgba(255,0,0,.7)",
+            boxParam: 4
+        });
+        setTimeout(()=>{
+            this.setState({
+                logInColor: "",
+                boxParam: 0,
+            });
+            setTimeout(()=>{
+                this.setState({
+                    logInStatus:""
+                })
+            },100)
+        },1500)
+    }
+
     render(){
+        const { boxParam,logInStatus,logInColor } = this.state;
         return (
-            <div className="center">
+            <div className="bigCard center">
                 <div className="formCard center">
                     <p className="titleHead">Welcome!</p>
                     <p className="labelIn">User</p>
@@ -53,6 +78,15 @@ class Signin extends React.Component {
                     onChange={this.onPassInput}
                     ></input>
                 <p className="signBtn signMargin" onClick={this.onButtonSubmit}><label className="signLabel">Sign In</label></p>
+                </div>
+                <div className="errCard center">
+                    <p className="errMess" 
+                    style={
+                        {
+                            boxShadow:`0 0 ${boxParam}px black`,
+                            backgroundColor: logInColor
+                        }
+                        }>{logInStatus}</p>
                 </div>
             </div>
         )
