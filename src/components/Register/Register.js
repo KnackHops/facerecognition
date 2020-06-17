3dda7ee0d703c7fbcb3bc66a6a2f095b5d54bc35
Aldrian <himalosc@gmail.com>
2020-06-17 14:55:34 +0800

    added backbone form register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,21 +1,58 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import '../Signin/Signin.css';
 
-const  Signin = ({onRouteChange}) => {
-    return (
-        <div className="center">
-            <div className="formCard regCard center">
-                <p className="titleHead">Register!</p>
-                <p className="labelIn">Name</p>
-                <input type="text"></input>
-                <p className="labelIn">Email</p>
-                <input type="email"></input>
-                <p className="labelIn">Password</p>
-                <input type="password"></input>
-            <p className="signBtn" onClick={()=>onRouteChange("home")}><label className="signLabel">Register</label></p>
+class Register extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            profile: {
+                regUsername: "",
+                regEmail: "",
+                regName: "",
+                regPassword: ""
+            } 
+        };
+    }
+
+    onButtonSubmit = () =>{
+        console.log(this.state)
+        const { regUsername, regEmail, regName, regPassword} = this.state.profile;
+        fetch('http://localhost:3000/register', {
+            method: "post",
+            headers: {"Content-Type":"application/json"},
+            body: JSON.stringify({
+                username:  regUsername,
+                name: regName,
+                email: regEmail,
+                password: regPassword
+            })
+        }).then(resp=>resp.json())
+        .then(data=>{
+            if(data.name===regName){
+                console.log(data)
+                this.props.onRouteChange("home")
+            }else{
+
+            }
+        })
+    }
+
+    render(){
+        return (
+            <div className="center">
+                <div className="formCard regCard center">
+                    <p className="titleHead">Register!</p>
+                    <p className="labelIn">Name</p>
+                    <input type="text"></input>
+                    <p className="labelIn">Email</p>
+                    <input type="email"></input>
+                    <p className="labelIn">Password</p>
+                    <input type="password"></input>
+                <p className="signBtn" onClick={this.onButtonSubmit}><label className="signLabel">Register</label></p>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
-export default Signin;
+export default Register;
