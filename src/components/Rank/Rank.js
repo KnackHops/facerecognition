fb22182f0833531a030cd45e2e80583280782281
Aldrian <himalosc@gmail.com>
2020-06-24 15:40:42 +0800

    Connected Rank.js to App.js

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,31 +1,17 @@
-import React, {Component} from 'react';
+import React from 'react';
 import "./Rank.css";
 
-class Rank extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            name: "",
-            entries: ""
-        }
-    }
-
-    componentDidMount(){
-        this.setState({name:this.props.name, rank:this.props.rank})
-    }
-
-    render(){
-        return (
-            <div>
-                <div className="center topLabel">
-                    {`${this.state.name}! Your current entry is...`}
-                </div>
-                <div className="center actualRank">
-                    {this.state.entries}
-                </div>
+const Rank =({name, entries})=>{
+    return (
+        <div>
+            <div className="center topLabel">
+                {`${name}! Your current entry is...`}
             </div>
-        )
-    }
+            <div className="center actualRank">
+                {entries}
+            </div>
+        </div>
+    )
 }
 
 export default Rank;
